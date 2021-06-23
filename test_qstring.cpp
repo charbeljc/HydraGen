40e9ed3add720cce01dd0ab42fa4f84666b4e5ca
Charbel Jacquin <charbel.jacquin@gmail.com>
2021-06-23 22:02:13 +0200

    fix code

--- test_qstring.cpp
+++ test_qstring.cpp
@@ -1,16 +1,16 @@
 #include <test_qstring.h>
 
 namespace bindtest {
-	QString hello_message_from_qt() {
+	QString TestQString::hello_message_from_qt() {
 		return QString("Hello from qt");
 	}
-	std::string hello_message_from_std() {
+	std::string TestQString::hello_message_from_std() {
 		return std:string("Hello from std");
 	}
-	void say_hello_with_qt(QString greetings) {
+	void TestQString::say_hello_with_qt(QString greetings) {
 		qDebug() << greetings;
 	}
-	void say_hello_with_std(std::string greetings) {
+	void TestQString::say_hello_with_std(std::string greetings) {
 		std::cerr << greetings;
 	}
 }
