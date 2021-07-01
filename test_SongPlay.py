dad23e8b6a61a9c496886dec1d6e1b1365c03cb4
Charbel Jacquin <charbel.jacquin@gmail.com>
2021-07-01 21:37:35 +0200

    it plays

---
+++ test_SongPlay.py
@@ -0,0 +1,16 @@
+from h2core import Logger, Object, Filesystem, Preferences, Hydrogen, Drumkit, Song
+import time
+
+# log_levels = Logger.log_levels
+logger = Logger.bootstrap(33)
+Object.bootstrap(logger, True)
+Filesystem.bootstrap(logger, "/usr/local/share/hydrogen/data/")
+prefs = Preferences.create_instance()
+hydrogen = Hydrogen.get_instance(False, True)
+dk = Drumkit.load_file("/home/rebelcat/.hydrogen/data/drumkits/RoRBateria/drumkit.xml", True)
+song = Song.load("/home/rebelcat/envelope_test.h2song")
+hydrogen.setSong(song)
+hydrogen.sequencer_play()
+time.sleep(60)
+hydrogen.sequencer_stop()
+print(song)
