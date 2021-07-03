2ab690967d4a6934a138b3b17275f09fa28bf3bf
Charbel Jacquin <charbel.jacquin@gmail.com>
2021-07-04 00:18:44 +0200

    add pybind11 as a requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import _install_setup_requires, setup
 setup(name='hydragen',
       version='0.1.0',
       packages=['hydra'],
-      install_requires=['logzero', 'orderedset', 'clang', 'PyYAML']
+      install_requires=['logzero', 'orderedset', 'clang', 'PyYAML', 'pybind11']
 )
 
